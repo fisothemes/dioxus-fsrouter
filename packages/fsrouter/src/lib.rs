75831b91b975da4f1a4d1cf4fa6cedd2e4262d39
Goodwill Mzumala <fisothemes@gmail.com>
2025-11-21 12:47:02 +0000

    feat: add `ParseError` enum for route parameter parsing with helper methods and tests

--- packages/fsrouter/src/lib.rs
+++ packages/fsrouter/src/lib.rs
@@ -12,15 +12,15 @@ pub use dioxus_fsrouter_macro as macros;
 #[doc(hidden)]
 pub use inventory;
 
-pub use errors::{RouterError, ValidationErrors};
+pub use errors::{ParseError, RouterError, ValidationErrors};
 pub use route::{RouteInfo, find_route, get_routes, validate_routes};
 pub use router::{Link, Navigation, Outlet, Router, use_navigation};
 
 pub mod prelude {
     pub use crate::macros::route;
     pub use crate::{
-        Link, Navigation, Outlet, Router, RouterError, ValidationErrors, get_routes, route,
-        route::validate_routes_or_panic, use_navigation, validate_routes,
+        Link, Navigation, Outlet, ParseError, Router, RouterError, ValidationErrors, get_routes,
+        route, route::validate_routes_or_panic, use_navigation, validate_routes,
     };
     #[doc(hidden)]
     pub use inventory;
